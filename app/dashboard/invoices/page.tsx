e1388047eaccf6834999046840a3fcb72c9aaa93
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2024-12-07 18:07:21 +0100

    chapter 13 finished

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,8 @@ import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
+
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
